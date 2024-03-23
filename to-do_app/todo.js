1ae0bb94eb0096f9f274cc35f9e903784caaaeff
thearyansingh <singharyan6999@gmail.com>
2024-03-24 00:28:17 +0530

    toDo list

---
+++ to-do_app/todo.js
@@ -0,0 +1,29 @@
+const item=document.querySelector("#item")
+const todoBox=document.querySelector("#todo")
+
+item.addEventListener(
+    "keyup",
+    function(event){
+        if(event.key=="Enter"){
+          addToDo(this.value)
+            this.value=""
+        }
+    }
+)
+const addToDo=(item)=>{
+    const list=document.createElement("li")
+    list.innerHTML=` ${item}  <i class="fas fa-times"></i>`;
+    list.addEventListener(
+        "click",
+        function(){
+           this.classList.toggle("done"); 
+        }
+    )
+    list.querySelector("i").addEventListener(
+        "click",
+        function(){
+           list.remove();
+        }
+    )
+    todoBox.appendChild(list)
+}
